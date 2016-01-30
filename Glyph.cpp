fbb2fd103d5236feb04535b9a410e4f1b584df90
Marcelino Coll <coll.marce@gmail.com>
2016-01-30 02:47:31 +0000

    Cansur

--- Glyph.cpp
+++ Glyph.cpp
@@ -3,49 +3,50 @@
 Glyph::Glyph(GlyphID id) {
   _id = id;
 
+  // CANSUR
   switch(id) {
-  glyph_UP:
-    _sprite = sf::Sprite(Resources::glyph_UP);
-  glyph_DOWN:
-    _sprite = sf::Sprite(Resources::glyph_DOWN);
-  glyph_RIGHT:
-    _sprite = sf::Sprite(Resources::glyph_RIGHT);
-  glyph_LEFT:
-    _sprite = sf::Sprite(Resources::glyph_LEFT);
-  glyph_11:
-    _sprite = sf::Sprite(Resources::glyph_11);
-  glyph_12:
-    _sprite = sf::Sprite(Resources::glyph_12);
-  glyph_13:
-    _sprite = sf::Sprite(Resources::glyph_13);
-  glyph_14:
-    _sprite = sf::Sprite(Resources::glyph_14);
-  glyph_15:
-    _sprite = sf::Sprite(Resources::glyph_15);
-  glyph_16:
-    _sprite = sf::Sprite(Resources::glyph_16);
-  glyph_17:
-    _sprite = sf::Sprite(Resources::glyph_17);
-  glyph_18:
-    _sprite = sf::Sprite(Resources::glyph_18);
-  glyph_19:
-    _sprite = sf::Sprite(Resources::glyph_19);
-  glyph_21:
-    _sprite = sf::Sprite(Resources::glyph_21);
-  glyph_22:
-    _sprite = sf::Sprite(Resources::glyph_22);
-  glyph_23:
-    _sprite = sf::Sprite(Resources::glyph_23);
-  glyph_24:
-    _sprite = sf::Sprite(Resources::glyph_24);
-  glyph_26:
-    _sprite = sf::Sprite(Resources::glyph_26);
-  glyph_31:
-    _sprite = sf::Sprite(Resources::glyph_31);
-  glyph_32:
-    _sprite = sf::Sprite(Resources::glyph_32);
-  glyph_33:
-    _sprite = sf::Sprite(Resources::glyph_33);
+  glyphUP:
+    _sprite = sf::Sprite(Resources::glyphUP);
+  glyphDOWN:
+    _sprite = sf::Sprite(Resources::glyphDOWN);
+  glyphRIGHT:
+    _sprite = sf::Sprite(Resources::glyphRIGHT);
+  glyphLEFT:
+    _sprite = sf::Sprite(Resources::glyphLEFT);
+  glyph11:
+    _sprite = sf::Sprite(Resources::glyph11);
+  glyph12:
+    _sprite = sf::Sprite(Resources::glyph12);
+  glyph13:
+    _sprite = sf::Sprite(Resources::glyph13);
+  glyph14:
+    _sprite = sf::Sprite(Resources::glyph14);
+  glyph15:
+    _sprite = sf::Sprite(Resources::glyph15);
+  glyph16:
+    _sprite = sf::Sprite(Resources::glyph16);
+  glyph17:
+    _sprite = sf::Sprite(Resources::glyph17);
+  glyph18:
+    _sprite = sf::Sprite(Resources::glyph18);
+  glyph19:
+    _sprite = sf::Sprite(Resources::glyph19);
+  glyph21:
+    _sprite = sf::Sprite(Resources::glyph21);
+  glyph22:
+    _sprite = sf::Sprite(Resources::glyph22);
+  glyph23:
+    _sprite = sf::Sprite(Resources::glyph23);
+  glyph24:
+    _sprite = sf::Sprite(Resources::glyph24);
+  glyph26:
+    _sprite = sf::Sprite(Resources::glyph26);
+  glyph31:
+    _sprite = sf::Sprite(Resources::glyph31);
+  glyph32:
+    _sprite = sf::Sprite(Resources::glyph32);
+  glyph33:
+    _sprite = sf::Sprite(Resources::glyph33);
   }
 }
 
